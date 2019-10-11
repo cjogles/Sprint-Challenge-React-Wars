8810b064f4d3add8771c6fae411d3884680e93ab
cjogles <jackson.ogles28@gmail.com>
2019-10-11 11:40:13 -0600

    finished MVP

--- starwars/src/components/StarWarsCard.js
+++ starwars/src/components/StarWarsCard.js
@@ -12,10 +12,12 @@ const StarWarsCard = (props) => {
             <Card>
             {(props.characters).map((num, index) => (
                 <CardBody key={index}>
-                    <CardImg key={index+1} top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
-                    <CardTitle key={index+2} >{props.characters[index].name}</CardTitle>
-                    <CardSubtitle key={index+3} > Birth Year: {props.characters[index].birth_year}</CardSubtitle>
-                    <CardText key={index+4} >{props.characters[index].films}</CardText>
+                    {/* <CardImg key={index+1} top width="100%" src="images\0.png" alt="Card image cap" /> */}
+                    <CardTitle key={index+1} >{props.characters[index].name}</CardTitle>
+                    <CardTitle key={index+2} >Gender: {props.characters[index].gender}</CardTitle>
+                    <CardTitle key={index+3} >Hair Color: {props.characters[index].hair_color}</CardTitle>
+                    <CardSubtitle key={index+4} > Birth Year: {props.characters[index].birth_year}</CardSubtitle>
+                    <CardText key={index+5  } >Film urls:<br></br> {props.characters[index].films}</CardText>
                 </CardBody>
             ))}
             </Card>
